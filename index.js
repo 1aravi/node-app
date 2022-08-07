58968a0adfadf0c33b27a21eb0a3f62c1003edcd
1aravi <aravindvasamsetti@gmail.com>
2022-08-08 00:17:46 +0530

    delete and update

--- index.js
+++ index.js
@@ -31,7 +31,14 @@ app.get('/', function (req, res) {
 
 //movies
 app.get('/movies', async function (request, response) {
-  const movies= await client.db("node-app").collection("movies").find({}).toArray();
+
+  if (request.query.rating){
+    request.query.rating= +request.query.rating;
+  }
+
+  console.log(request.query);
+
+  const movies= await client.db("node-app").collection("movies").find(request.query).toArray();
     response.send(movies);
   })
 
@@ -59,6 +66,32 @@ app.post('/movies', async function (request, response) {
 
   const result = await client.db("node-app").collection("movies").insertMany(data);
 response.send(result);
+});
+
+// movies delete
+app.delete('/movies/:id', async function (request, response) {
+
+  const {id}=request.params;
+  console.log(request.params, id);
+  //db.movies.deleteOne({id: "101"})
+
+  // const movie = movies.find((mv) => mv.id === id);
+  const result = await client.db("node-app").collection("movies").deleteOne({id: id});
+
+  console.log(result);
+  result.deletedCount > 0 ? response.send({msg: "Movie deleted successfully"}): response.status(404).send({msg: "Movie not found"});
+});
+
+//movies update
+ app.put('/movies/:id', async function (request, response) {
+    const {id}=request.params;
+    console.log(request.params, id);
+    const data=request.body;
+
+    //db.movies.updateOne({id: "101"}, {$set: data})
+
+    const result = await client.db("node-app").collection("movies").updateOne({id: id}, {$set: data});
+    response.send(result);
 });
 
 app.listen(PORT, ()=>console.log(`App started in ${PORT}`))
