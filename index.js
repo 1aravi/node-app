925dbee6f43a35f816696cce471a3e2b2ca3b093
1aravi <aravindvasamsetti@gmail.com>
2022-08-11 23:36:09 +0530

    Cors added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import {moviesRouter} from "./routes/movies.js"
-import {cors} from "cors";
+import cors from "cors";
 
 dotenv.config();
 // console.log(process.env.MONGO_URL);
